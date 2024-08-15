2141ecfb66c6c5bba4de6d892993bbe568154a18
Aleksei Alekseev <angelpsymail@gmail.com>
2024-08-15 22:21:05 +0400

    lift state of form up

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,17 +1,20 @@
 import styles from "./styles.module.css";
 
 type Props = {
+  id: number;
   author: string;
   body: string;
   className?: string;
   htmlTag?: React.ElementType;
+  selected?: boolean;
+  onSelect?: (id: number) => void;
 };
 
 export default function Post(props: Props) {
-  const classNameOfRoot = props.className ? `${styles.root} ${props.className}` : styles.card;
+  const classNameOfRoot = styles.root + (props.selected ? ` ${styles.selected}` : "") + (props.className ? ` ${props.className}` : "");
   const Tag = props.htmlTag || "li";
   return (
-    <Tag className={classNameOfRoot}>
+    <Tag className={classNameOfRoot} onClick={() => props.onSelect?.(props.id)}>
       <p className={styles.author}>{props.author}</p>
       <p className={styles.body}>{props.body}</p>
     </Tag>
--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -1,22 +1,70 @@
+import { useState } from 'react';
 import NewPost from "../new-post";
 import Post from "../post";
 import styles from "./styles.module.css";
 
 export default function Posts() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      author: 'Aleksei',
+      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, impedit repudiandae alias ex nisi dolor perferendis sequi autem distinctio doloremque fugit delectus debitis magni doloribus? Odit ab corporis eos quisquam.',
+    },
+    {
+      id: 2,
+      author: 'Aleksey',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non, libero!',
+    },
+  ]);
+  const [body, setBody] = useState('');
+  const [name, setName] = useState('');
+  const [idSelectedPost, setIdSelectedPost] = useState<null | number>(null);
+
+  function handleChangeBody(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setBody(event.target.value);
+  }
+
+  function handleChangeName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  function selectPost(id: number) {
+    if (idSelectedPost === id) {
+      setIdSelectedPost(null);
+      setBody('');
+      setName('');
+      return;
+    }
+    setIdSelectedPost(id);
+    const post = items.find((item) => item.id === id);
+    if (post) {
+      setBody(post.body);
+      setName(post.author);
+    }
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        body={body}
+        name={name}
+        onChangeBody={handleChangeBody}
+        onChangeName={handleChangeName}
+      />
       <ul className={styles.root}>
-        <Post
-          className={styles.card}
-          author="Aleksei"
-          body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, impedit repudiandae alias ex nisi dolor perferendis sequi autem distinctio doloremque fugit delectus debitis magni doloribus? Odit ab corporis eos quisquam."
-        />
-        <Post
-          className={styles.card}
-          author="Aleksey"
-          body="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non, libero!"
-        />
+        {items.map((item) => {
+          return (
+            <Post
+              className={styles.card}
+              key={item.id}
+              id={item.id}
+              author={item.author}
+              body={item.body}
+              selected={idSelectedPost === item.id}
+              onSelect={(id) => selectPost(id)}
+            />
+          )
+        })}
       </ul>
     </>
   );
