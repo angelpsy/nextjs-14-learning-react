8723694d220277e5561c206e1493c6041c3c86bd
Aleksei Alekseev <angelpsymail@gmail.com>
2024-08-15 21:59:46 +0400

    start use of component state

--- src/components/new-post/index.tsx
+++ src/components/new-post/index.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import classes from './styles.module.css'
 
 export default function NewPost() {
+  const [body, setBody] = useState('');
+  const [name, setName] = useState('');
+
+  function handleChangeBody(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setBody(event.target.value);
+  }
+
+  function handleChangeName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <div className={classes.field}>
         <label htmlFor="body">Body</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" value={body} required rows={3} onChange={handleChangeBody} />
       </div>
       <div className={classes.field}>
         <label htmlFor="name">Author's name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" value={name} required onChange={handleChangeName}/>
       </div>
     </form>
   )
