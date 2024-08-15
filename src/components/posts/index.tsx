029ce9999b056d5a7e9a1e60c75f7bec0d292889
Aleksei Alekseev <angelpsymail@gmail.com>
2024-08-15 21:27:23 +0400

    add base component of form

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -1,19 +1,23 @@
-import Post from '../post'
-import styles from './styles.module.css'
+import NewPost from "../new-post";
+import Post from "../post";
+import styles from "./styles.module.css";
 
 export default function Posts() {
   return (
-    <ul className={styles.root}>
-      <Post
-        className={styles.card}
-        author="Aleksei"
-        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, impedit repudiandae alias ex nisi dolor perferendis sequi autem distinctio doloremque fugit delectus debitis magni doloribus? Odit ab corporis eos quisquam."
-      />
-      <Post
-        className={styles.card}
-        author="Aleksey"
-        body="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non, libero!"
-      />
-    </ul>
-  )
+    <>
+      <NewPost />
+      <ul className={styles.root}>
+        <Post
+          className={styles.card}
+          author="Aleksei"
+          body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, impedit repudiandae alias ex nisi dolor perferendis sequi autem distinctio doloremque fugit delectus debitis magni doloribus? Odit ab corporis eos quisquam."
+        />
+        <Post
+          className={styles.card}
+          author="Aleksey"
+          body="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non, libero!"
+        />
+      </ul>
+    </>
+  );
 }
